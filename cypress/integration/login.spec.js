2ce19939e82e4cc36c958f1b212935dd9a15c135
Lucas Bertucini <lucas_bertucini@hotmail.com>
2023-04-10 20:48:27 -0300

    alterations

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,17 @@
+import 'cypress-file-upload'
+import 'cypress-iframe'
+import 'cypress-mailosaur'
+
+// cypress/integration/login.spec.js
+
+it('successfully logs in', () => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+
+  cy.visit('/login')
+  cy.get('#email').type(Cypress.env('USER_EMAIL'))
+  cy.get('#password').type(Cypress.env('USER_PASSWORD'))
+  cy.contains('button', 'Login').click()
+
+
+  cy.contains('h1', 'Your Notes').should('be.visible')
+})
