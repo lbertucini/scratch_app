a03ce0c7301295175fd4cb8a1a32b185da97a644
Lucas Bertucini <lucas_bertucini@hotmail.com>
2023-04-12 17:32:18 -0300

    ci testing
    ci testing

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -11,7 +11,5 @@ it('successfully logs in', () => {
   cy.get('#email').type(Cypress.env('USER_EMAIL'))
   cy.get('#password').type(Cypress.env('USER_PASSWORD'))
   cy.contains('button', 'Login').click()
-
-
   cy.contains('h1', 'Your Notes').should('be.visible')
 })
